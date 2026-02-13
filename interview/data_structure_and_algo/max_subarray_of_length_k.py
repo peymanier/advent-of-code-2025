d1c9e042653b08d0aff7b147893848457b1711bb
Peyman <hassani.peyman627@gmail.com>
2026-02-13 14:55:46 +0330

    Add max subarray of length k alternate solution

--- interview/data_structure_and_algo/max_subarray_of_length_k.py
+++ interview/data_structure_and_algo/max_subarray_of_length_k.py
@@ -1,7 +1,7 @@
 def max_subarray_of_length(nums: list[int], k: int) -> int:
     window = nums[0:k]
     result = sum(window)
-    for right in range(k, len(nums)):
+    for right in range(k + 1, len(nums)):
         left = right - k
         window = nums[left:right]
         result = max(result, sum(window))
@@ -9,10 +9,28 @@ def max_subarray_of_length(nums: list[int], k: int) -> int:
     return result
 
 
+def max_subarray_of_length2(nums: list[int], k: int) -> int:
+    window = nums[0:k]
+    window_sum = sum(window)
+
+    result = window_sum
+    for right in range(k, len(nums)):
+        left = right - k
+        window_sum -= nums[left]
+        window_sum += nums[right]
+        result = max(result, window_sum)
+
+    return result
+
+
 def main():
     nums = [1, 2, 3, 7, 4, 1]
     k = 3
-    result = max_subarray_of_length(nums, k)
+
+    # result = max_subarray_of_length(nums, k)
+    # print(result)
+
+    result = max_subarray_of_length2(nums, k)
     print(result)
 
 
