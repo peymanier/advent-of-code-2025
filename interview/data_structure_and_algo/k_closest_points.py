bbc61df659f72d124ce88298bdd807747a33078a
Peyman <hassani.peyman627@gmail.com>
2026-02-13 22:16:43 +0330

    Add k closes points solution

---
+++ interview/data_structure_and_algo/k_closest_points.py
@@ -0,0 +1,24 @@
+import heapq
+
+
+def k_closest_points(points: list[list[int]], k: int) -> list[list[int]]:
+    heap = []
+    for x, y in points:
+        heapq.heappush(heap, (x**2 + y**2, [x, y]))
+
+    result = []
+    for _ in range(k):
+        result.append(heapq.heappop(heap)[1])
+
+    return result
+
+
+def main():
+    points = [[3, 3], [5, -1], [-2, 4], [0, 6]]
+    k = 2
+    result = k_closest_points(points, k)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
