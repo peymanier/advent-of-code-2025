69d4772eb0a5fc44317b1481ebb98a47d0a9a6e6
Peyman <hassani.peyman627@gmail.com>
2026-02-13 14:32:38 +0330

    Add middle of linked list solution

---
+++ interview/data_structure_and_algo/middle_of_a_linked_list.py
@@ -0,0 +1,33 @@
+from interview.data_structure_and_algo.linked_list import (
+    Node,
+    build_linked_list,
+    print_linked_list,
+)
+
+
+def middle_of_linked_list(head: Node) -> int:
+    fast = head
+    slow = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow.val
+
+
+def main():
+    head = build_linked_list([0, 1, 2, 3, 4])
+    print_linked_list(head)
+
+    result = middle_of_linked_list(head)
+    print(result)
+
+    head = build_linked_list([0, 1, 2, 3, 4, 5])
+    print_linked_list(head)
+
+    result = middle_of_linked_list(head)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
