16ba648a7c1f2d82d86228cea42b69f1af16838d
Peyman <hassani.peyman627@gmail.com>
2026-02-13 13:19:18 +0330

    Add valid_palindrome solution

---
+++ interview/data_structure_and_algo/valid_palindrome.py
@@ -0,0 +1,42 @@
+def is_palindrome(s: str) -> bool:
+    s = "".join([ch.lower() for ch in s if ch.isalnum()])
+    left = 0
+    right = len(s) - 1
+    while left < right:
+        if s[left] != s[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+def is_palindrome2(s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+        while l < r and not s[l].isalnum():
+            l += 1
+
+        while l < r and not s[r].isalnum():
+            r -= 1
+
+        if s[l].lower() != s[r].lower():
+            return False
+
+        l += 1
+        r -= 1
+
+    return True
+
+
+def main():
+    s = "A man, a plan, a canal: Panama"
+    # s = "race a car"
+    # result = is_palindrome(s)
+    result = is_palindrome2(s)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
