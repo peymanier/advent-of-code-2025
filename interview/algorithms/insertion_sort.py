694fda8e1a5a769d6e0370e27686f28ba88310dd
Peyman <hassani.peyman627@gmail.com>
2026-02-14 13:22:20 +0330

    Add insertion sort algo

---
+++ interview/algorithms/insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(nums: list[int]) -> list[int]:
+    for i in range(1, len(nums)):
+        j = i
+        while j > 0 and nums[j - 1] > nums[j]:
+            nums[j - 1], nums[j] = nums[j], nums[j - 1]
+            j -= 1
+
+    return nums
+
+
+def main():
+    nums = [8, 2, 8, 1, 3, 9, 5]
+    result = insertion_sort(nums)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
