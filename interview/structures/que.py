cc19e2f42f8955445c6e39ed8fd61f8c2e2d6ad5
Peyman <hassani.peyman627@gmail.com>
2026-03-04 17:34:04 +0330

    Add queue implementation using linked list

--- interview/structures/que.py
+++ interview/structures/que.py
@@ -36,3 +36,50 @@ class Queue:
             return None
 
         return self.items[0]
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class QueueLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        curr = self.head
+        while curr:
+            yield curr
+            curr = curr.next
+
+    def add_to_head(self, node):
+        if not self.head:
+            self.head = node
+            self.tail = node
+            return
+
+        node.next = self.head
+        self.head = node
+
+    def add_to_tail(self, node):
+        if not self.tail:
+            self.head = node
+            self.tail = node
+            return
+
+        self.tail.next = node
+        self.tail = node
+
+
+if __name__ == "__main__":
+    ll = QueueLinkedList()
+    ll.add_to_tail(Node(5))
+    ll.add_to_tail(Node(6))
+    ll.add_to_head(Node(0))
+    ll.add_to_head(Node(-1))
+
+    for node in ll:
+        print(node.val)
