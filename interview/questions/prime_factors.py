75a00542ba6685b81e5d509ef43938a22a70e4e5
Peyman <hassani.peyman627@gmail.com>
2026-03-06 19:54:45 +0330

    Update prime factors solution again

--- interview/questions/prime_factors.py
+++ interview/questions/prime_factors.py
@@ -8,7 +8,7 @@ def prime_factors(n):
         factors.append(2)
         n = n // 2
 
-    for i in range(3, int(math.sqrt(n))):
+    for i in range(3, int(math.sqrt(n)), 2):
         while n % i == 0:
             n = n // i
             factors.append(i)
