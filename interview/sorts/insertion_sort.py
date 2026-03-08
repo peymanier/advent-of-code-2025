3e5f61b2bdda3365531a9400227d7843304b6d59
Peyman <hassani.peyman627@gmail.com>
2026-03-08 12:01:57 +0330

    Update insertion sort

--- interview/sorts/insertion_sort.py
+++ interview/sorts/insertion_sort.py
@@ -8,9 +8,21 @@ def insertion_sort(nums: list[int]) -> list[int]:
     return nums
 
 
+def insertion_sort2(nums: list[int]) -> list[int]:
+    for i in range(1, len(nums)):
+        for j in range(i, 0, -1):
+            if nums[j - 1] > nums[j]:
+                nums[j - 1], nums[j] = nums[j], nums[j - 1]
+
+    return nums
+
+
 def main():
     nums = [8, 2, 8, 1, 3, 9, 5]
     result = insertion_sort(nums)
+    print(result)
+
+    result = insertion_sort2(nums)
     print(result)
 
 
