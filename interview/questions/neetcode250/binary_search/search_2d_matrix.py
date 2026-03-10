88630cc22f9e7d2a06d200ef5596d51d09b43c21
Peyman <hassani.peyman627@gmail.com>
2026-03-10 15:41:57 +0330

    Add search 2d matrix alt solution

--- interview/questions/neetcode250/binary_search/search_2d_matrix.py
+++ interview/questions/neetcode250/binary_search/search_2d_matrix.py
@@ -18,6 +18,43 @@ def search_2d_matrix(matrix: list[list[int]], target: int) -> bool:
     return False
 
 
+def search_2d_matrix_alt(matrix: list[list[int]], target: int) -> bool:
+    left = 0
+    right = len(matrix)
+
+    row_index = None
+    while left <= right:
+        mid = (left + right) // 2
+        low = matrix[mid][0]
+        high = matrix[mid][-1]
+        if low <= target <= high:
+            row_index = mid
+            break
+
+        if target < matrix[mid][0]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    if not row_index:
+        return False
+
+    row = matrix[row_index]
+    left = 0
+    right = len(row)
+    while left <= right:
+        mid = (left + right) // 2
+        if row[mid] == target:
+            return True
+
+        if target < row[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return False
+
+
 def main():
     matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
     target = 34
@@ -25,9 +62,17 @@ def main():
     print("expected", True, end=" ")
     print("got", result)
 
+    result = search_2d_matrix_alt(matrix, target)
+    print("expected", True, end=" ")
+    print("got", result)
+
     matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
     target = 8
     result = search_2d_matrix(matrix, target)
+    print("expected", False, end=" ")
+    print("got", result)
+
+    result = search_2d_matrix_alt(matrix, target)
     print("expected", False, end=" ")
     print("got", result)
 
