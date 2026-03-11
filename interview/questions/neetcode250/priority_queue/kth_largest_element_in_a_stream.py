e21ffe7aaa706cb2bb05e7dab4c4e6b8b0c5456f
Peyman <hassani.peyman627@gmail.com>
2026-03-11 13:14:31 +0330

    Add kth_largest_element_in_a_stream.py alt

--- interview/questions/neetcode250/priority_queue/kth_largest_element_in_a_stream.py
+++ interview/questions/neetcode250/priority_queue/kth_largest_element_in_a_stream.py
@@ -13,8 +13,28 @@ class KthLargest:
         return heapq.nlargest(self.k, self.heap)[-1]
 
 
+class KthLargestAlt:
+    def __init__(self, k, elements):
+        self.k = k
+        self.heap = []
+
+        for elem in elements:
+            self._append(elem)
+
+    def _append(self, val: int) -> None:
+        heapq.heappush(self.heap, val)
+
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        self._append(val)
+        return self.heap[0]
+
+
 def main():
-    kth_largest = KthLargest(3, [4, 5, 8, 2])
+    # kth_largest = KthLargest(3, [4, 5, 8, 2])
+    kth_largest = KthLargestAlt(3, [4, 5, 8, 2])
 
     result = kth_largest.add(3)
     print("expected", 4, "got", result)
