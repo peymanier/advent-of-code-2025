9c65561f7781084fabe7a1cdb6040d9b601e6c57
Peyman <hassani.peyman627@gmail.com>
2026-03-13 19:36:35 +0330

    Add sort_array.py

---
+++ interview/questions/neetcode250/arrays_and_hashing/sort_array.py
@@ -0,0 +1,85 @@
+# def merge(nums1, nums2):
+#     result = []
+#
+#     i = 0
+#     j = 0
+#     while i < len(nums1) and j < len(nums2):
+#         if nums1[i] <= nums2[j]:
+#             result.append(nums1[i])
+#             i += 1
+#         else:
+#             result.append(nums2[j])
+#             j += 1
+#
+#     if i < len(nums1):
+#         result.extend(nums1[i:])
+#
+#     if j < len(nums2):
+#         result.extend(nums2[j:])
+#
+#     return result
+#
+#
+# def sort_array(nums: list[int]) -> list[int]:
+#     if len(nums) <= 1:
+#         return nums
+#
+#     mid = len(nums) // 2
+#     left = sort_array(nums[:mid])
+#     right = sort_array(nums[mid:])
+#     return merge(left, right)
+
+
+def merge(arr: list[int], l: int, m: int, r: int):
+    nums1 = arr[l : m + 1]
+    nums2 = arr[m + 1 : r + 1]
+    k = l
+
+    i = 0
+    j = 0
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] <= nums2[j]:
+            arr[k] = nums1[i]
+            i += 1
+        else:
+            arr[k] = nums2[j]
+            j += 1
+
+        k += 1
+
+    while i < len(nums1):
+        arr[k] = nums1[i]
+        i += 1
+        k += 1
+
+    while j < len(nums2):
+        arr[k] = nums2[j]
+        j += 1
+        k += 1
+
+    return
+
+
+def sort_array(nums: list[int]) -> list[int]:
+    def merge_sort(arr: list[int], l: int, r: int):
+        if l == r:
+            return arr
+
+        m = (l + r) // 2
+        merge_sort(arr, l, m)
+        merge_sort(arr, m + 1, r)
+        merge(arr, l, m, r)
+        return arr
+
+    return merge_sort(nums, 0, len(nums) - 1)
+
+
+def main():
+    nums = [5, 2, 3, 1]
+    result = sort_array(nums)
+    val = [1, 2, 3, 5]
+    print("passed:", result == val, "expected", val, "got", result)
+
+
+if __name__ == "__main__":
+    main()
