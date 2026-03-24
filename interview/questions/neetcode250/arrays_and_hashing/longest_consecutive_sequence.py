5e8cb628bad308f067feec673b78cab68a8e51e3
Peyman <hassani.peyman627@gmail.com>
2026-03-24 16:28:02 +0330

    Add longest_consecutive_sequence.py alternative

--- interview/questions/neetcode250/arrays_and_hashing/longest_consecutive_sequence.py
+++ interview/questions/neetcode250/arrays_and_hashing/longest_consecutive_sequence.py
@@ -12,9 +12,33 @@ def longest_consecutive_sequence(nums: list[int]) -> list[int]:
     return result
 
 
+def longest_consecutive_sequence_alt(nums: list[int]) -> list[int]:
+    nums = set(nums)
+    result = (0, 0)
+    for n in nums:
+        if n - 1 in nums:
+            continue
+
+        count = 1
+        i = n + 1
+        while i in nums:
+            count += 1
+            i += 1
+
+        result = max(result, (count, n))
+
+    counter, start = result
+    return [i for i in range(start, start + counter)]
+
+
 def main():
     nums = [100, 4, 200, 1, 3, 2]
     result = longest_consecutive_sequence(nums)
+    val = [1, 2, 3, 4]
+    print("passed:", result == val, "expected", val, "got", result)
+
+    nums = [100, 4, 200, 1, 3, 2]
+    result = longest_consecutive_sequence_alt(nums)
     val = [1, 2, 3, 4]
     print("passed:", result == val, "expected", val, "got", result)
 
