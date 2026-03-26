c721c04bf8fba9aa563274c1c5ff652c739b8d43
Peyman <hassani.peyman627@gmail.com>
2026-03-26 11:24:08 +0330

    Update merge_sorted_array.py

--- interview/questions/neetcode250/two_pointers/merge_sorted_array.py
+++ interview/questions/neetcode250/two_pointers/merge_sorted_array.py
@@ -1,33 +1,60 @@
+# def merge_sorted_arrays(nums1, nums2):
+#     i, j, k = 0, 0, 0
+#     left = nums1[: len(nums1) - len(nums2)]
+#     right = nums2
+#     while i < len(left) and j < len(right):
+#         if left[i] <= right[j]:
+#             nums1[k] = left[i]
+#             i += 1
+#         else:
+#             nums1[k] = right[j]
+#             j += 1
+#
+#         k += 1
+#
+#     while i < len(left):
+#         nums1[k] = left[i]
+#         i += 1
+#         k += 1
+#
+#     while j < len(right):
+#         nums1[k] = right[j]
+#         j += 1
+#         k += 1
+#
+#     return nums1
+
+
 def merge_sorted_arrays(nums1, nums2):
-    i, j, k = 0, 0, 0
-    left = nums1[: len(nums1) - len(nums2)]
-    right = nums2
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            nums1[k] = left[i]
-            i += 1
+    i = len(nums1) - len(nums2) - 1
+    j = len(nums2) - 1
+    k = len(nums1) - 1
+    while i >= 0 and j >= 0:
+        if nums1[i] >= nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
         else:
-            nums1[k] = right[j]
-            j += 1
+            nums1[k] = nums2[j]
+            j -= 1
 
-        k += 1
+        k -= 1
 
-    while i < len(left):
-        nums1[k] = left[i]
-        i += 1
-        k += 1
+    while i >= 0:
+        nums1[k] = nums1[i]
+        i -= 1
+        k -= 1
 
-    while j < len(right):
-        nums1[k] = right[j]
-        j += 1
-        k += 1
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
 
     return nums1
 
 
 def main():
-    nums1 = [1, 2, 3, 0, 0, 0]
-    nums2 = [2, 5, 6]
+    nums1 = [2, 2, 3, 0, 0, 0]
+    nums2 = [1, 5, 6]
     result = merge_sorted_arrays(nums1, nums2)
     val = [1, 2, 2, 3, 5, 6]
     print("passed:", result == val, "expected", val, "got", result)
