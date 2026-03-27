3e97a11b94c9cc865f762051d8d96254fc50318c
Peyman <hassani.peyman627@gmail.com>
2026-03-28 01:27:33 +0330

    Update sum_of_all_subsets_xor_totals.py

--- interview/questions/neetcode250/backtracking/sum_of_all_subsets_xor_totals.py
+++ interview/questions/neetcode250/backtracking/sum_of_all_subsets_xor_totals.py
@@ -1,29 +1,39 @@
+# def sum_of_all_subsets_xor_totals(nums: list[int]) -> int:
+#     subsets = []
+#
+#     def find_subsets(subset, i):
+#         if i == len(nums):
+#             subsets.append(subset)
+#             return
+#
+#         include = [nums[i], *subset]
+#         not_include = [*subset]
+#
+#         find_subsets(include, i + 1)
+#         find_subsets(not_include, i + 1)
+#         return
+#
+#     find_subsets([], 0)
+#
+#     result = 0
+#     for subset in subsets:
+#         xor_sum = 0
+#         for num in subset:
+#             xor_sum ^= num
+#
+#         result += xor_sum
+#
+#     return result
+
+
 def sum_of_all_subsets_xor_totals(nums: list[int]) -> int:
-    subsets = []
-
-    def find_subsets(subset, i):
+    def dfs(i, total):
         if i == len(nums):
-            subsets.append(subset)
-            return
+            return total
 
-        include = [nums[i], *subset]
-        not_include = [*subset]
+        return dfs(i + 1, total ^ nums[i]) + dfs(i + 1, total)
 
-        find_subsets(include, i + 1)
-        find_subsets(not_include, i + 1)
-        return
-
-    find_subsets([], 0)
-
-    result = 0
-    for subset in subsets:
-        xor_sum = 0
-        for num in subset:
-            xor_sum ^= num
-
-        result += xor_sum
-
-    return result
+    return dfs(0, 0)
 
 
 def main():
