19178db1a85ea6f501a2b6222801774a5d47db66
Peyman <hassani.peyman627@gmail.com>
2026-03-30 13:46:28 +0330

    Update maximum_sum_circular_subarray.py

--- interview/questions/neetcode250/backtracking/maximum_sum_circular_subarray.py
+++ interview/questions/neetcode250/backtracking/maximum_sum_circular_subarray.py
@@ -1,21 +1,69 @@
+# def maximum_sum_circular_subarray(nums: list[int]) -> int | None:
+#     result = float("-inf")
+#
+#     def dfs(start: int, curr: int, curr_sum: int):
+#         nonlocal result
+#         result = max(result, curr_sum)
+#
+#         nxt = (curr + 1) % len(nums)
+#         if start == nxt:
+#             return
+#
+#         dfs(start, nxt, curr_sum + nums[curr])
+#         return
+#
+#     for k in range(len(nums)):
+#         dfs(k, k, 0)
+#
+#     return result if result != float("-inf") else None
+
+
+# def maximum_sum_circular_subarray(nums: list[int]) -> int | None:
+#     result = 0
+#     for i in range(len(nums)):
+#         curr_sum = nums[i]
+#         j = (i + 1) % len(nums)
+#         result = max(result, curr_sum)
+#
+#         while j != i:
+#             curr_sum += nums[j]
+#             result = max(result, curr_sum)
+#             j = (j + 1) % len(nums)
+#
+#     return result
+
+
+# def maximum_sum_circular_subarray(nums: list[int]) -> int | None:
+#     result = float("-inf")
+#     curr_sum = 0
+#     nums = nums * 2
+#     nums.pop()
+#
+#     for i in range(len(nums)):
+#         if nums[i] > curr_sum:
+#             curr_sum = nums[i]
+#         else:
+#             curr_sum += nums[i]
+#
+#         result = max(result, curr_sum)
+#
+#     return result
+
+
 def maximum_sum_circular_subarray(nums: list[int]) -> int | None:
     result = float("-inf")
+    curr_sum = 0
 
-    def dfs(start: int, curr: int, curr_sum: int):
-        nonlocal result
+    for k in range(2 * len(nums) - 1):
+        i = k % len(nums)
+        if nums[i] > curr_sum:
+            curr_sum = nums[i]
+        else:
+            curr_sum += nums[i]
+
         result = max(result, curr_sum)
 
-        nxt = (curr + 1) % len(nums)
-        if start == nxt:
-            return
-
-        dfs(start, nxt, curr_sum + nums[curr])
-        return
-
-    for k in range(len(nums)):
-        dfs(k, k, 0)
-
-    return result if result != float("-inf") else None
+    return result
 
 
 def main():
