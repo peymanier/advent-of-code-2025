57be1e91d844adb72f67e78cac51dfcd3227a289
Peyman <hassani.peyman627@gmail.com>
2026-03-30 16:24:45 +0330

    Add test case for maximum_sum_circular_subarray.py

--- interview/questions/neetcode250/backtracking/maximum_sum_circular_subarray.py
+++ interview/questions/neetcode250/backtracking/maximum_sum_circular_subarray.py
@@ -77,6 +77,11 @@ def main():
     val = 10
     print("passed:", result == val, "expected", val, "got", result)
 
+    nums = [-5, -3, -5]
+    result = maximum_sum_circular_subarray(nums)
+    val = -3
+    print("passed:", result == val, "expected", val, "got", result)
+
 
 if __name__ == "__main__":
     main()
