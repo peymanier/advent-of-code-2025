106d7de91b9d8736dccb99627b923993bb76b6c9
Peyman <hassani.peyman627@gmail.com>
2026-04-03 16:46:48 +0330

    Update happy_number.py

--- interview/questions/neetcode250/math_and_geometry/happy_number.py
+++ interview/questions/neetcode250/math_and_geometry/happy_number.py
@@ -1,21 +1,51 @@
+# def is_happy_number(num: int) -> bool:
+#     digits = list(str(num))
+#     visited = set()
+#     while True:
+#         digits_squared = 0
+#         for _ in range(len(digits)):
+#             curr = digits.pop()
+#             digits_squared += int(curr) ** 2
+#
+#         if digits_squared == 1:
+#             return True
+#
+#         if digits_squared in visited:
+#             return False
+#
+#         visited.add(digits_squared)
+#
+#         for digit in list(str(digits_squared)):
+#             digits.append(digit)
+#
+
+
+def get_digits_of_num(num: int) -> list[int]:
+    digits = []
+    while num > 0:
+        digits.append(num % 10)
+        num //= 10
+
+    return digits
+
+
 def is_happy_number(num: int) -> bool:
-    digits = list(str(num))
+    digits = get_digits_of_num(num)
     visited = set()
     while True:
-        digits_squared = 0
+        curr = 0
         for _ in range(len(digits)):
-            curr = digits.pop()
-            digits_squared += int(curr) ** 2
+            curr += digits.pop() ** 2
 
-        if digits_squared == 1:
+        if curr == 1:
             return True
 
-        if digits_squared in visited:
+        if curr in visited:
             return False
 
-        visited.add(digits_squared)
+        visited.add(curr)
 
-        for digit in list(str(digits_squared)):
+        for digit in get_digits_of_num(curr):
             digits.append(digit)
 
 
