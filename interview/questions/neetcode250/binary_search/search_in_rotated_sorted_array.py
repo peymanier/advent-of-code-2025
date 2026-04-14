f7e49bef6ce074cd09730f586c17f84428670131
Peyman <hassani.peyman627@gmail.com>
2026-04-14 10:35:39 +0330

    Add search_in_rotated_sorted_array.py

---
+++ interview/questions/neetcode250/binary_search/search_in_rotated_sorted_array.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+def search(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] >= nums[left]:
+            if target > nums[mid] or target < nums[left]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        else:
+            if target < nums[mid] or target > nums[right]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test1(self):
+        nums = [4, 5, 6, 7, 0, 1, 2]
+        target = 0
+        got = search(nums, target)
+        expected = 4
+        self.assertEqual(expected, got)
+
+    def test2(self):
+        nums = [4, 5, 6, 7, 0, 1, 2]
+        target = 3
+        got = search(nums, target)
+        expected = -1
+        self.assertEqual(expected, got)
+
+
+if __name__ == "__main__":
+    unittest.main()
