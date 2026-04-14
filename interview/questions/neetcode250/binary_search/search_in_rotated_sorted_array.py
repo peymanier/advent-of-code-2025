b446863dfeb9f2e216b966eb5776af23fc858282
Peyman <hassani.peyman627@gmail.com>
2026-04-14 11:22:18 +0330

    Update search_in_rotated_sorted_array.py

--- interview/questions/neetcode250/binary_search/search_in_rotated_sorted_array.py
+++ interview/questions/neetcode250/binary_search/search_in_rotated_sorted_array.py
@@ -11,15 +11,15 @@ def search(nums: list[int], target: int) -> int:
             return mid
 
         if nums[mid] >= nums[left]:
-            if target > nums[mid] or target < nums[left]:
-                left = mid + 1
-            else:
+            if nums[left] <= target < nums[mid]:
                 right = mid - 1
+            else:
+                left = mid + 1
         else:
-            if target < nums[mid] or target > nums[right]:
-                right = mid - 1
-            else:
+            if nums[mid] < target <= nums[right]:
                 left = mid + 1
+            else:
+                right = mid - 1
 
     return -1
 
