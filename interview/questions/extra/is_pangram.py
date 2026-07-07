9b2e9c584e7368678f37c56e16d70e559c29ae2b
Peyman <hassani.peyman627@gmail.com>
2026-07-07 12:39:50 +0330

    Add is_pangram.py alt

--- interview/questions/extra/is_pangram.py
+++ interview/questions/extra/is_pangram.py
@@ -13,9 +13,26 @@ def is_pangram(s: str) -> bool:
     return all(alphabets)
 
 
+def is_pangram2(s: str) -> bool:
+    if len(s) < 26:
+        return False
+
+    bits = 0
+    for char in s:
+        if not char.isalpha():
+            continue
+
+        pos = ord(char.lower()) - ord("a")
+        bits |= 1 << pos
+
+    target = (1 << 26) - 1
+    return bits == target
+
+
 def main():
     candidate = "The quick brown fox jumps over the lazy dog."
     print(is_pangram(candidate))
+    print(is_pangram2(candidate))
 
 
 if __name__ == "__main__":
